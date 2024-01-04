216c6f8aa6d628498afa8e3cf6c60a5e98edbb85
Satyam-G5 <satyamshukla4916@gmail.com>
2024-01-04 20:54:13 +0530

    Database

--- Backend/Database/postgres.ts
+++ Backend/Database/postgres.ts
@@ -3,21 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-const pool = new Pool({
-    host: 'localhost',
-    port: 5433,
-    database: 'talkitive',
-    user: 'postgres',
-    password: 'Satyam12'
-});
-
-// const dblocal = `postgresql://${process.env.PGLOCAL_USER}:${process.env.PGLOCAL_PASSWORD}@${process.env.PGLOCAL_HOST}:${process.env.PGLOCAL_PORT}/${process.env.PGLOCAL_DATABASE}`
- 
 // const pool = new Pool({
-//     connectionString : process.env.NODE_ENV === "production" ? process.env.POSTGRES_URL  : dblocal, 
-//     ssl : {
-//       rejectUnauthorized : false  
-//     }
-//   })
+//     host: 'localhost',
+//     port: 5433,
+//     database: 'talkitive',
+//     user: 'postgres',
+//     password: 'Satyam12'
+// });
+
+const dblocal = `postgresql://${process.env.PGLOCAL_USER}:${process.env.PGLOCAL_PASSWORD}@${process.env.PGLOCAL_HOST}:${process.env.PGLOCAL_PORT}/${process.env.PGLOCAL_DATABASE}`
+ 
+const pool = new Pool({
+    connectionString : process.env.NODE_ENV === "production" ? process.env.POSTGRES_URL  : dblocal, 
+    ssl : {
+      rejectUnauthorized : false  
+    }
+  })
 
 export default pool;
